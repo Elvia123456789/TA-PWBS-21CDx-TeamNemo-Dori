41676052007197dfcd416fd995a1421af13a3d74
Elvia123456789 <elviaharianja@gmail.com>
2024-06-02 21:12:35 +0700

    commit ke-2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(Register::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
 });

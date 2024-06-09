e9adcc29be14230707369e441fad2cb618958317
Elvia123456789 <elviaharianja@gmail.com>
2024-06-09 21:05:22 +0700

    Update Register Ke-3

--- backend/app/Http/Controllers/Register.php
+++ backend/app/Http/Controllers/Register.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class Register extends Controller
 {
-    //
+    public function register(Request $request) : JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if($validator->fails()) {
+            return $this->send
+        }
+    }
 }

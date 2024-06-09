940b1f80c7da7c503d0c91abb62fd6b9325faef7
Limah19 <muslimahnur999@gmail.com>
2024-06-09 23:48:03 +0700

    update page pakan

--- frontend/app/pakan/page.tsx
+++ frontend/app/pakan/page.tsx
@@ -9,7 +9,7 @@ export default function PakanPage() {
             {/* area menu */}
             <nav className="flex justify-center bg-gradient-to-r from-gray-300 to-sky-300 p-3 shadow-lg">
                 <Link
-                    href={"/view"}
+                    href={"/"}
                     className="flex justify-center mr-1 px-5 py-3 w-50 rounded-full text-white active:bg-sky-300 active:text-grey w-2/12"
                 >
                     Home
@@ -41,7 +41,29 @@ export default function PakanPage() {
 
             </nav>
 
-            {/* area home */}
+            {/* area data pakan */}
+            <div className="container mx-auto p-8">
+                <h1 className="text-4xl font-bold mb-4">Data Pakan</h1>
+                <div className="bg-white rounded-lg shadow p-6">
+                    <table className="min-w-full bg-white">
+                        <thead>
+                            <tr>
+                                <th className="py-2 px-4 border-b-2 border-gray-200">Nama Pakan</th>
+                                <th className="py-2 px-4 border-b-2 border-gray-200">Jumlah</th>
+                                <th className="py-2 px-4 border-b-2 border-gray-200">Jenis</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="py-2 px-4 border-b border-gray-200"></td>
+                                <td className="py-2 px-4 border-b border-gray-200"></td>
+                                <td className="py-2 px-4 border-b border-gray-200"></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     )
 }

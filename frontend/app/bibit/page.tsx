43bac50cc4c19eb2b9726a3553281c6ab5cd6d1e
Limah19 <muslimahnur999@gmail.com>
2024-06-09 21:00:34 +0700

    update page view

--- frontend/app/bibit/page.tsx
+++ frontend/app/bibit/page.tsx
@@ -8,12 +8,12 @@ export default function BibitPage() {
         <div>
             {/* area menu */}
             <nav className="flex justify-center bg-gradient-to-r from-gray-300 to-sky-300 p-3 shadow-lg">
-            <Link
+                <Link
                     href={"/view"}
                     className="flex justify-center mr-1 px-5 py-3 w-50 rounded-full text-white active:bg-sky-300 active:text-grey w-2/12"
                 >
                     Home
-                    </Link>
+                </Link>
                 <a
                     href={"/kolam"}
                     className="flex justify-center mr-1 px-5 py-3 w-50 rounded-full text-white active:bg-sky-300 active:text-grey w-2/12"
@@ -38,7 +38,7 @@ export default function BibitPage() {
                 >
                     Pemesanan
                 </a>
-                
+
             </nav>
 
             {/* area home */}

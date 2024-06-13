6217146e52d96606ef17a9c06479777b4cf19753
Elvia123456789 <elviaharianja@gmail.com>
2024-06-13 22:15:16 +0700

    Update Models Ke-3

---
+++ backend/app/Models/MLogout.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MLogout extends Model
+{
+    use HasFactory;
+}

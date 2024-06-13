815666a1eaf808e479b11f8484c6c7105f146f07
Elvia123456789 <elviaharianja@gmail.com>
2024-06-13 21:50:31 +0700

    Commit Ke-9

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // Registrasi pengguna baru
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'data' => $user,
+            'message' => 'User registered successfully'
+        ], 201);
+    }
+}
+

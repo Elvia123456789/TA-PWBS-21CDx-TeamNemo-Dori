8d77d17ecaaf2d07912bdec779797276aa352fdf
Elvia123456789 <elviaharianja@gmail.com>
2024-06-15 21:47:43 +0700

    Commit Ke-8

--- backend/app/Models/MKolam.php
+++ backend/app/Models/MKolam.php
@@ -16,6 +16,20 @@ class MKolam extends Model
          $query = $this->select("id AS id_kolam", "nama AS nama_kolam", "ukuran AS ukuran_kolam", "jenis AS jenis_kolam")->from($this->table)->orderBy("id");
  
          return $query->get();
-        }  
+        }
+
+         // buat fungsi untuk pencarian data mahasiswa
+    function searchData($keyword)
+    {
+        $query = $this->select("id AS id_kolam", "nama AS nama_kolam", "ukuran AS ukuran_kolam", "jenis AS jenis_kolam")->from($this->table)
+        // pencarian nama, ukuran dan jenis harus sesuai dengan data
+        ->orWhere('ukuran', $keyword)
+        ->orWhere('jenis', $keyword)
+        // pencarian nama sesuai dengan karakter yang ada pada data
+        ->orWhereRaw('nama LIKE ?',["%$keyword%"])
+        ->orderBy("id");
+
+        return $query->get();
+    }  
 
 }

ccff18e9b326166d109474f2c3d05fd48dbb2c67
Limah19 <muslimahnur999@gmail.com>
2024-06-16 11:23:58 +0700

    update migration, model, controller datakolam

--- backend/app/Models/MDataKolam.php
+++ backend/app/Models/MDataKolam.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MKolam extends Model
+class MDataKolam extends Model
 {
     use HasFactory;
 }

153c888e2e4b3f504094fb86d407bbf2738b1460
Elvia123456789 <elviaharianja@gmail.com>
2024-06-16 21:47:12 +0700

    Update Controllers Ke-7

--- backend/app/Http/Controllers/Kolam.php
+++ backend/app/Http/Controllers/Kolam.php
@@ -16,4 +16,25 @@ class Kolam extends Controller
         // isi nilai variabel "$model" 
         $this->model = new MKolam();
     }
+
+     // buat fungsi untuk tambah data
+     function viewData()
+     {
+         // jika data mahasiswa tidak ada
+         if (count($this->model->viewData()) == 0) {
+             $data = [];
+             $error = 1;
+             $message = "Data Kolam Tidak Ditemukan !";
+         }
+         // jika data kolam ada / tersedia
+         else {
+             // ambil method "viewData" dari model "MKolam"
+             $data = $this->model->viewData();
+             $error = 0;
+             $message = "";
+         }
+ 
+         return response(["kolam" => $data, "error" => $error, "message" => $message], http_response_code());
+     }
+
 }
